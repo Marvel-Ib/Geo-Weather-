f32ed1ca834a815efbee43a51901344946a76f8e
Marvel-Ib <awesomewonder553@gmail.com>
2020-03-05 15:29:01 +0100

    done

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const express = require('express');
 const hbs = require('hbs')
-const port = 9000 || process.env.PORT
 const app = express();
 const bodyparser  = require('body-parser');
 
@@ -26,11 +25,5 @@ app.get('/getweather',control.gweather);
 app.use(control.error)
 
 
-app.listen(port)
+app.listen(process.env.PORT || 5000)
 
-//forward geocoding
-//   https://api.mapbox.com/geocoding/v5/mapbox.places/paris.json?access_token=pk.eyJ1IjoibWFydmVsLWliIiwiYSI6ImNrMTNodGFodDA3N2IzaXJxcmxnNTI5ZTMifQ.UKeLhyp30PL9cwIkDUwDAw&limit=1
-
-
-//reverse geocoding 
-//  https://api.mapbox.com/geocoding/v5/mapbox.places/-122.463,37.7648.json?access_token=pk.eyJ1IjoibWFydmVsLWliIiwiYSI6ImNrMTNodGFodDA3N2IzaXJxcmxnNTI5ZTMifQ.UKeLhyp30PL9cwIkDUwDAw&limit=1
